309cb780988ad6f2a5f7237d0ae94177725ce632
Anton2156 <akkforwork11@gmain.com>
2021-02-12 21:42:36 +0200

    gds

---
+++ DZ.5/5.2.py
@@ -0,0 +1,16 @@
+def main(): 
+    print(
+        "Enter '+' if you want to add \
+        \nEnter '-' if you want to subtraction \
+        \nEnter '*' if you want to multiplication \
+        \nEnter '/' if you want to division"
+        )
+    actoin = str(input("Enter the actoin: "))
+    if actoin == "+":
+        pass
+    elif actoin == "-":
+        pass
+    elif actoin == "*":
+        pass
+    elif actoin == "/":
+        pass
